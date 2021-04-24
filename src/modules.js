3abdca658df570272685c27302ef07cb899a80e9
chinmaykarmokar <chinmaykarmokar@gmail.com>
2021-04-24 19:19:52 +0530

    changed email

--- src/modules.js
+++ src/modules.js
@@ -3,6 +3,6 @@ module.exports = {
     user: "sql6406059",
     password: "xS6yR7ljMf",
     database: "sql6406059",
-    email: "chinmaykarmokar@gmail.com",
+    email: "resultly@gmail.com",
     pass: "!champion"
 }
