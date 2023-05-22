a2c661d0f94cbdb2c14ed1b81f876750b1f084f4
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2023-05-22 13:17:14 +0530

    Made changes to the DB endpoints.

--- src/modules.js
+++ src/modules.js
@@ -1,8 +1,8 @@
 module.exports = {
-    host: "ec2-52-206-193-199.compute-1.amazonaws.com",
-    user: "fzeduekrhbkoml",
-    password: "931835e12acbdd388d36eb03c644df626f649ee79893e521ce867dbd82f80ace",
-    database: "d3jl1ecplgceat",
+    host: "heffalump.db.elephantsql.com",
+    user: "gcpdilnn",
+    password: "0jT5e4a3HKcHKQ2Glv75GVX1igOwLDBQ",
+    database: "gcpdilnn",
     email: "covinfoindia@gmail.com",
     pass: "!champion",
     name: "Chinmay Karmokar"
